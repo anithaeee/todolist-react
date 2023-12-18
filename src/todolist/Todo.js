3a785d716c4ecd982c7bb5baf392815ce237380d
anithaeee <anithaeee224@gmail.com>
2023-12-18 21:28:49 +0800

    dec18th

--- src/todolist/Todo.js
+++ src/todolist/Todo.js
@@ -1,9 +1,30 @@
-import React from 'react'
-import './todolist/todo.css';
+import React, { useState } from 'react'
+import '../todolist/todo.css';
+import 'bootstrap/dist/css/bootstrap.css';
 const Todo = () => {
+    const[Task,setTask]= useState([]);
+    const changeHandler = e=> {
+        setTask(e.target.value)
+    }
+    const submitHandler = e=>{
+        e.preventDefault();
+        console.log(Task);
+    }
   return (
-    <div>
-
+    <div className='container'>
+            <div className='card-text'>
+              <h1 className='heading'style={{padding:'3rem'}}>TODO LISI</h1>
+           </div>
+           <div className='btn'>
+             <button className='button'style={{backgroundColor:'blue',borderRadius:'6px',color:'white',height:'35px',width:'100px'}}>Add Task</button>
+             <button className='button1'style={{backgroundColor:'blue',borderRadius:'6px',color:'white',height:'35px',width:'100px'}}>All</button>
+           </div>
+           <div className='card' style={{height:'20rem',width:'40rem',marginRight:'80rem'}}>
+            <form onSubmit={submitHandler}>
+            <input type='text' name='Task' placeholder='create a react project'value={Task} onChange={changeHandler}></input>
+            <input type='submit' placeholder='create a react project'></input>
+            </form>
+           </div>
     </div>
   )
 }
